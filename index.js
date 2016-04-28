8f03ba0bef87ab06f2003789b192b3ec37cbb16d
Santiago Ferreira <san650@gmail.com>
2016-04-27 22:46:33 -0300

    Resolve scopes

--- index.js
+++ index.js
@@ -1,8 +1,99 @@
 !function() {
+  function merge() {
+    var target = arguments[0];
+    var sources = Array.prototype.slice.call(arguments, 1);
+    var source;
+
+    for(var i = 0; i < sources.length; i++) {
+      source = sources[i];
+
+      if (!source) {
+        continue;
+      }
+
+      for(var attr in source) {
+        if (source.hasOwnProperty(attr) && !/^[0-9]+$/.test(attr)) {
+          target[attr] = source[attr];
+        }
+      }
+    }
+
+    return target;
+  }
+
+  function getPath(node) {
+    var values = [],
+        current = node;
+
+    if (node.length) {
+      values.push(node);
+    }
+
+    while(current = Ceibo.parent(current)) {
+      if (current.__scope) {
+        values.unshift(current.__scope);
+      }
+    }
+
+    return values;
+  }
+
+  var FIRST;
+  function buildObject(node, blueprintKey, blueprint) {
+    var value;
+
+    if (FIRST && blueprint.visit) {
+      FIRST = false;
+      value = new String(blueprint.visit);
+      delete blueprint.visit;
+      Ceibo.defineProperty(node, blueprintKey, value);
+    } else {
+      value = new String(blueprint.scope || '');
+      value.__scope = blueprint.scope;
+      value.__blueprint = blueprint;
+      delete blueprint.scope;
+
+      Ceibo.defineProperty(node, blueprintKey, undefined, function() {
+        var path = getPath(node);
+
+        if (value.length) {
+          path.push(value + '');
+        }
+
+        var newvalue = new String(path.join(' '));
+        merge(newvalue, value);
+
+        return newvalue;
+      });
+    }
+
+    return [value, blueprint];
+  }
+
+  function buildString(node, blueprintKey, str) {
+    var value = new String(str);
+
+    Ceibo.defineProperty(node, blueprintKey, undefined, function() {
+      var path = getPath(node);
+      path.push(value);
+
+      return path.join(' ');
+    });
+  }
 
   var Ombu = {};
 
-  Ombu.create = function create() {
+  Ombu.create = function create(definition) {
+    FIRST = true;
+
+    var tree = Ceibo.create(definition || {}, {
+      builder: {
+        object: buildObject,
+        string: buildString
+      }
+    });
+
+    return tree;
   };
 
   if (typeof define === 'function') {
--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,69 @@
 module('Unit');
 
-test('returns a string', function(assert) {
-  var tree = Ombu.create('/path');
+test('uses `visit` as root string', function(assert) {
+  var tree = Ombu.create({
+    visit: '/path'
+  });
 
-  assert.equal(tree, 'value');
+  assert.equal(tree, '/path');
+});
+
+test('leaf nodes return a string', function(assert) {
+  var tree = Ombu.create({
+    foo: '.a-foo'
+  });
+
+  assert.equal(tree.foo, '.a-foo');
+});
+
+test('appends scope to leafs', function(assert) {
+  var tree = Ombu.create({
+    foo: {
+      scope: '.a-foo',
+
+      bar: '.a-bar'
+    }
+  });
+
+  assert.equal(tree.foo, '.a-foo');
+  assert.equal(tree.foo.bar, '.a-foo .a-bar');
+});
+
+test('appends scope to nodes', function(assert) {
+  var tree = Ombu.create({
+    foo: {
+      scope: '.a-foo',
+
+      bar: {
+        scope: '.a-bar',
+
+        baz: '.a-baz'
+      }
+    }
+  });
+
+  assert.equal(tree.foo.bar, '.a-foo .a-bar');
+  assert.equal(tree.foo.bar.baz, '.a-foo .a-bar .a-baz');
+});
+
+test('it works', function(assert) {
+  var tree = Ombu.create({
+    foo: {
+      scope: '.a-foo',
+
+      bar: {
+        scope: '.a-bar',
+
+        baz: {
+          qux: '.a-qux'
+        }
+      }
+    }
+  });
+
+  assert.equal(tree, '');
+  assert.equal(tree.foo, '.a-foo');
+  assert.equal(tree.foo.bar, '.a-foo .a-bar');
+  assert.equal(tree.foo.bar.baz, '.a-foo .a-bar');
+  assert.equal(tree.foo.bar.baz.qux, '.a-foo .a-bar .a-qux');
 });
